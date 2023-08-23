6b62cedaa80589fff6e41bb7715eadbb650fabdc
LakshayaT <lakshaya.t@osmosys.co>
2023-08-23 16:22:03 +0530

    feat: implement typeorm to connect to database
    Add TypeORM configuration and options for connecting to database in app.module
    Configuration values for DB connection are specified in .env, based on .env.example

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DatabaseConfiguration } from './config/database/configuration';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: DatabaseConfiguration,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
