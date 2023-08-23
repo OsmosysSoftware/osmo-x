bb948711e52b8e91e902c603cb3a1fdc3073d266
LakshayaT <108812833+LakshayaT@users.noreply.github.com>
2023-08-23 18:32:32 +0530

    feat: configure TypeORM to connect to Database (#4)

---
+++ src/config/database/configuration.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class DatabaseConfiguration implements TypeOrmOptionsFactory {
+  constructor(private configService: ConfigService) {}
+
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: 'mysql',
+      host: this.configService.get<string>('DB_HOST'),
+      port: +this.configService.get<number>('DB_PORT'),
+      username: this.configService.get<string>('DB_USERNAME'),
+      password: this.configService.get<string>('DB_PASSWORD'),
+      database: this.configService.get<string>('DB_NAME'),
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: false,
+    };
+  }
+}
