d5d46db60503781093a4af356ee169fffe9cf5a4
xixas <vikaskyatannawar@gmail.com>
2023-08-28 21:56:31 +0530

    feat: add ESLint rules for code quality improvement (#12)
    Co-authored-by: Vikas Kyatannawar <vikas.k@osmosys.co>

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,8 +18,21 @@ module.exports = {
   ignorePatterns: ['.eslintrc.js'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-function-return-type': ['error', { allowExpressions: true }],
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-unused-vars': 'error',
+    'no-console': 'warn',
+    'padding-line-between-statements': [
+      'error',
+      { 'blankLine': 'always', 'prev': '*', 'next': 'function' },
+      { 'blankLine': 'always', 'prev': 'function', 'next': '*' },
+      { 'blankLine': 'always', 'prev': '*', 'next': 'if' },
+      { 'blankLine': 'always', 'prev': 'if', 'next': '*' },
+      { 'blankLine': 'always', 'prev': '*', 'next': 'for' },
+      { 'blankLine': 'always', 'prev': 'for', 'next': '*' },
+      { 'blankLine': 'always', 'prev': '*', 'next': 'while' },
+      { 'blankLine': 'always', 'prev': 'while', 'next': '*' }
+    ]
   },
 };
