d30e032f6b0f06aea8b589d80596a2eb3c5d4e9d
Vikas Kyatannawar <vikas.k@osmosys.co>
2023-09-01 16:15:02 +0530

    refactor: add redis host & port from env, add error on failed connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ScheduleModule } from '@nestjs/schedule';
 import { DatabaseConfiguration } from './config/database/configuration';
 import { NotificationsModule } from './models/notifications/notifications.module';
 import { SmtpService } from './services/email/smtp/smtp.service';
+import { BullModule } from '@nestjs/bull';
 
+const configService = new ConfigService();
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    BullModule.forRoot({
+      redis: {
+        host: configService.getOrThrow<string>('REDIS_HOST'),
+        port: +configService.getOrThrow<number>('REDIS_PORT'),
+      },
+    }),
     ScheduleModule.forRoot(),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
