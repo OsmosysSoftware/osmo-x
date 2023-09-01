f59e28055b3cb64a7dbe71739a66ccb27e9e4a6c
xixas <vikaskyatannawar@gmail.com>
2023-09-01 16:46:02 +0530

    feat: manage redis configuration (#33)

--- src/jobs/producers/notifications/notifications.job.producer.ts
+++ src/jobs/producers/notifications/notifications.job.producer.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { InjectQueue } from '@nestjs/bull';
 import { Queue } from 'bull';
 import { Notification } from 'src/models/notifications/entities/notification.entity';
@@ -6,7 +6,13 @@ import { Notification } from 'src/models/notifications/entities/notification.ent
 @Injectable()
 export class NotificationQueueProducer {
   constructor(@InjectQueue('smtpNotifications') private readonly smtpQueue: Queue) {}
-
+  private readonly logger = new Logger(NotificationQueueProducer.name);
+  async onModuleInit(): Promise<void> {
+    this.smtpQueue.client.on('error', (error) => {
+      this.logger.error('Redis connection error:');
+      this.logger.error(JSON.stringify(error, ['message', 'stack', 2]));
+    });
+  }
   async addNotificationToQueue(notification: Notification): Promise<void> {
     await this.smtpQueue.add(notification.id);
   }
