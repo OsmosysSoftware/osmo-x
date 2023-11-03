9d1431d84b21aa1ba4c1c2361349e1c1d0eb032a
Lakshaya <lakshaya.t@osmosys.co>
2023-11-03 18:24:14 +0530

    Update script for resetting database
    The script will connect to the database, drop the migrations and notifications table and re-run the migrations

--- src/database/database-reset.ts
+++ src/database/database-reset.ts
@@ -1,20 +1,37 @@
 import DataSource from '../config/typeorm/configuration';
 import { QueryRunner } from 'typeorm';
+import { Logger } from '@nestjs/common';
 
-async function resetDatabase(): Promise<void> {
-  const dataSource = DataSource;
-  const queryRunner: QueryRunner = dataSource.createQueryRunner();
+const logger = new Logger('DatabaseReset');
 
-  try {
-    await queryRunner.connect();
-    await queryRunner.dropTable('notify_notifications', true);
-    await queryRunner.clearTable('notify_migrations');
-  } catch (error) {
-    process.stderr.write(`${error}\n`);
+DataSource.initialize()
+  .then(async () => {
+    logger.log('Data source initialized successfully');
+
+    const queryRunner: QueryRunner = DataSource.createQueryRunner();
+
+    try {
+      await queryRunner.connect();
+
+      logger.log('Dropping notifications and migrations tables...');
+      await queryRunner.dropTable('notify_notifications', true);
+      await queryRunner.dropTable('notify_migrations', true);
+      logger.log('Dropped notifications and migrations tables successfully');
+
+      logger.log('Running migrations...');
+      await DataSource.runMigrations();
+      logger.log('Running migrations completed successfully');
+
+      logger.log('Database reset completed successfully');
+      process.exit(0);
+    } catch (error) {
+      logger.error(`Error while resetting database: ${error}`);
+      process.exit(1);
+    } finally {
+      await queryRunner.release();
+    }
+  })
+  .catch((error) => {
+    logger.error(`Error while initializing data source: ${error}`);
     process.exit(1);
-  } finally {
-    await queryRunner.release();
-  }
-}
-
-resetDatabase();
+  });
