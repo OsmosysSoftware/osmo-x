e44fdc2568f3355531acba7edf7f584e2c1784ad
PrinceYadav2 <prince.y@osmosys.co>
2023-12-01 14:20:09 +0530

    Fix lint errors for prod environment file

--- apps/portal/src/environments/environment.prod.ts
+++ apps/portal/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    production: true,
-    graphqlEndpoint: 'http://localhost:3000/graphql',
-    serverApiKey: 'osmo-notify-test-key',
-  };
+  production: true,
+  graphqlEndpoint: 'http://localhost:3000/graphql',
+  serverApiKey: 'osmo-notify-test-key',
+};
