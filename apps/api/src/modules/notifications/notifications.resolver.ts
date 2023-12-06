469e84836be1b3c4a9cfb5baad288dd0434bdca7
PrinceYadav2 <114974207+PrinceYadav2@users.noreply.github.com>
2023-12-06 13:08:15 +0530

    feat: make fetch all notifications a public API (#83)

--- apps/api/src/modules/notifications/notifications.resolver.ts
+++ apps/api/src/modules/notifications/notifications.resolver.ts
@@ -1,11 +1,13 @@
 import { Query, Resolver } from '@nestjs/graphql';
 import { NotificationsService } from './notifications.service';
 import { Notification } from './entities/notification.entity';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { ApiKeyGuard } from 'src/common/guards/api-key/api-key.guard';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { UseGuards } from '@nestjs/common';
 
 @Resolver(() => Notification)
-@UseGuards(ApiKeyGuard)
+// @UseGuards(ApiKeyGuard)
 export class NotificationsResolver {
   constructor(private readonly notificationsService: NotificationsService) {}
 
