576c93f4336cbb86ae2bd56aebe46e7561893d47
Lakshaya <lakshaya.t@osmosys.co>
2024-01-11 18:21:44 +0530

    refactor: perform lint fixes

--- apps/portal/src/app/auth/login/login.component.ts
+++ apps/portal/src/app/auth/login/login.component.ts
@@ -77,6 +77,7 @@ export class LoginComponent implements OnInit {
             this.router.navigate(['notifications']);
           }
         }
+
         this.loading = false;
       },
       error: (error) => {
