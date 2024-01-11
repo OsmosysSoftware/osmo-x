747a13d7b2bb9e236eb9be2b79c18a5818ef4978
Lakshaya <lakshaya.t@osmosys.co>
2024-01-11 18:09:12 +0530

    feat: add login page with auth guard
    Create a new login component, add the required logic in login.component ts file for methods and making required method calls for logging in, creating error toasts
    Add the required html code and styles for creating a login form with basic validations on username and password, showing any login errors that occur
    Create a new auth.guard ts file for checking is user is logged in before accessing notifications page, redirect accordingly
    Add an auth.interface file for specifying the LoginRequestBody and ErrorResponse
    Create an auth.service file for checking if isUserLogged in and call graphql methods for loginUser
    Update graphql.queries to add new LoginUser query
    Update graphql.service to use the token obtained after logging in for graphql query
    Update app-routing module to add the login path, navigate and activate auth guard accordingly
    Update app.module and primeng.module to import and add required modules and services
    Adde the primeflex package in package.json and update styles.scss to import the primeflex sheet for properly showing login page

---
+++ apps/portal/src/app/auth/login/login.component.ts
@@ -0,0 +1,93 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
+import { ErrorResponse, LoginRequestBody } from '../auth.interface';
+import { AuthService } from '../auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+
+  loading = false;
+
+  private fb: FormBuilder = new FormBuilder();
+
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private messageService: MessageService,
+  ) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      selectedUsername: ['', [Validators.required]],
+      selectedPassword: ['', [Validators.required]],
+    });
+  }
+
+  get selectedUsername() {
+    return this.loginForm.get('selectedUsername');
+  }
+
+  get selectedPassword() {
+    return this.loginForm.get('selectedPassword');
+  }
+
+  loginWithUsername() {
+    this.loading = true;
+    const body: LoginRequestBody = {
+      username: this.selectedUsername.value,
+      password: this.selectedPassword.value,
+    };
+    this.authService.loginUser(body).subscribe({
+      next: (resp) => {
+        if (resp.errors) {
+          let errorMessage: string;
+
+          if (resp.errors && resp.errors.length > 0) {
+            const error = resp.errors[0] as ErrorResponse;
+            errorMessage =
+              error.extensions?.originalError?.message ||
+              error.message ||
+              'An error occurred while logging in';
+          } else {
+            errorMessage = 'An error occurred while logging in';
+          }
+
+          if (Array.isArray(errorMessage)) {
+            [errorMessage] = errorMessage;
+          }
+
+          this.messageService.add({
+            key: 'tst',
+            severity: 'error',
+            summary: 'Error',
+            detail: errorMessage,
+          });
+        } else {
+          localStorage.setItem('osmoNotifyUserData', JSON.stringify(resp.data.login));
+          localStorage.setItem('osmoNotifyLoggedAt', new Date().toISOString());
+
+          if (this.router.url !== '/notifications') {
+            this.router.navigate(['notifications']);
+          }
+        }
+        this.loading = false;
+      },
+      error: (error) => {
+        this.messageService.add({
+          key: 'tst',
+          severity: 'error',
+          summary: 'Error',
+          detail: error,
+        });
+        this.loading = false;
+      },
+    });
+  }
+}
