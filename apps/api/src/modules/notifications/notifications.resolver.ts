5d4a6e2de602e9b13c00ba4f7573670a2e3582ff
xixas <vikaskyatannawar@gmail.com>
2024-01-17 11:19:22 +0530

    feat: add pagination support to graphql query (#113)

--- apps/api/src/modules/notifications/notifications.resolver.ts
+++ apps/api/src/modules/notifications/notifications.resolver.ts
@@ -1,18 +1,23 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Query, Resolver } from '@nestjs/graphql';
 import { NotificationsService } from './notifications.service';
 import { Notification } from './entities/notification.entity';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { ApiKeyGuard } from 'src/common/guards/api-key/api-key.guard';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { UseGuards } from '@nestjs/common';
+import { NotificationResponse } from './dtos/notification-response.dto';
+import { QueryOptionsDto } from './dtos/query-options.dto';
 
 @Resolver(() => Notification)
 @UseGuards(ApiKeyGuard)
 export class NotificationsResolver {
   constructor(private readonly notificationsService: NotificationsService) {}
 
-  @Query(() => [Notification], { name: 'notifications' })
-  async findAll(): Promise<Notification[]> {
-    return this.notificationsService.getAllNotifications();
+  @Query(() => NotificationResponse, { name: 'notifications' })
+  async findAll(
+    @Args('options', { type: () => QueryOptionsDto, nullable: true, defaultValue: {} })
+    options: QueryOptionsDto,
+  ): Promise<NotificationResponse> {
+    return this.notificationsService.getAllNotifications(options);
   }
 }
