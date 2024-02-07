86c75025976630aef8696dec43efd61ea979f09c
Lakshaya <lakshaya.t@osmosys.co>
2024-02-07 18:27:30 +0530

    style: fix linting issues

--- apps/portal/src/app/app.component.spec.ts
+++ apps/portal/src/app/app.component.spec.ts
@@ -26,8 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'OsmoX app is running!',
-    );
+    expect(compiled.querySelector('.content span')?.textContent).toContain('OsmoX app is running!');
   });
 });
