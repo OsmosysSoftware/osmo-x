9cdf0974f4f881bfc36d0200bd875158ee845e66
Lakshaya <lakshaya.t@osmosys.co>
2024-03-05 17:29:28 +0530

    feat: add max length limit for 'to' and 'message'

--- apps/api/src/modules/notifications/dtos/providers/smsTwilio-data.dto.ts
+++ apps/api/src/modules/notifications/dtos/providers/smsTwilio-data.dto.ts
@@ -1,9 +1,13 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, MaxLength } from 'class-validator';
 
 export class SmsTwilioDataDto {
   @IsNotEmpty()
+  // Max length as per E.164 (https://www.twilio.com/docs/glossary/what-e164)
+  @MaxLength(16)
   to: string;
 
   @IsNotEmpty()
+  // Max length as per https://www.twilio.com/docs/glossary/what-sms-character-limit
+  @MaxLength(1600)
   message: string;
 }
