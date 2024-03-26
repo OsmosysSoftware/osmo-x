58db589137027ee2aaa0a2a3041825c06a292b99
harish <harish@osmosys.co>
2024-03-26 10:15:38 +0530

    feat: add swagger

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -6,6 +6,7 @@ import * as fs from 'fs';
 import { loggerConfig } from './config/logger.config';
 import { JsendFormatter } from './common/jsend-formatter';
 import { HttpExceptionFilter } from './common/http-exception.filter';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 const logDir = 'logs';
 
@@ -19,6 +20,14 @@ async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule, {
     logger: loggerConfig,
   });
+  const config = new DocumentBuilder()
+    .setTitle('OsmosX')
+    .setDescription('The Osmos-x API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpExceptionFilter(new JsendFormatter()));
   // TODO: Update origin as needed
