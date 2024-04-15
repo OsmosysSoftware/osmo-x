933a5c5f57181ef07b0702fde2d889dcea72ade3
kshitij.k <kshitij.k@osmosys.co>
2024-04-15 13:12:17 +0530

    refactor: remove seeding and unused dto

--- apps/api/src/modules/server-api-keys/dto/update-server-api-key.input.ts
+++
@@ -1,8 +0,0 @@
-import { CreateServerApiKeyInput } from './create-server-api-key.input';
-import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
-
-@InputType()
-export class UpdateServerApiKeyInput extends PartialType(CreateServerApiKeyInput) {
-  @Field(() => Int)
-  id: number;
-}
