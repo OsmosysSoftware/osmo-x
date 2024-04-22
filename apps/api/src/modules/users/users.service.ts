4ab893e07efde6ca762c132dde0a668fef75529a
kshitij-k-osmosys <121787214+kshitij-k-osmosys@users.noreply.github.com>
2024-04-22 09:28:06 +0530

    feat: update authorization logic (#206)

---
+++ apps/api/src/modules/users/users.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { User } from './entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async findByUsername(username: string): Promise<User | undefined> {
+    return this.userRepository.findOne({ where: { username } });
+  }
+}
