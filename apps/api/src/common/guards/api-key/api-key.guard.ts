44c096cec4b5167a06430390fc6d191b8a81ef70
kshitij.k <kshitij.k@osmosys.co>
2024-04-22 13:37:11 +0530

    feat: guard uses db values

--- apps/api/src/common/guards/api-key/api-key.guard.ts
+++ apps/api/src/common/guards/api-key/api-key.guard.ts
@@ -1,30 +1,51 @@
 import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { GqlExecutionContext } from '@nestjs/graphql';
 import { Observable } from 'rxjs';
+import { ServerApiKeysService } from 'src/modules/server-api-keys/server-api-keys.service';
 
-const configService = new ConfigService();
 @Injectable()
 export class ApiKeyGuard implements CanActivate {
+  constructor(private readonly serverApiKeysService: ServerApiKeysService) {}
+
   canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
-    const apiKey = configService.getOrThrow<string>('SERVER_API_KEY');
-    const request = context.switchToHttp().getRequest();
+    return this.validateRequest(context);
+  }
+
+  async validateRequest(execContext: ExecutionContext): Promise<boolean> {
+    const request = execContext.switchToHttp().getRequest();
+    let apiKeyToken = null;
 
     // Get auth header incase of http request
     if (request && request.headers) {
       const authHeader = request.headers['authorization'];
 
-      if (authHeader && authHeader === `Bearer ${apiKey}`) {
+      if (authHeader.startsWith('Bearer ')) {
+        apiKeyToken = authHeader.substring(7);
+      } else {
+        throw new Error('Invalid bearer token format');
+      }
+
+      const apiKeyEntry = await this.serverApiKeysService.findByServerApiKey(apiKeyToken);
+
+      if (apiKeyToken && apiKeyToken === apiKeyEntry.apiKey) {
         return true;
       }
     }
 
     // Get quth header incase of graphql request
-    const ctx = GqlExecutionContext.create(context);
+    const ctx = GqlExecutionContext.create(execContext);
     const req = ctx.getContext().req;
     const authHeader = req.headers.authorization;
 
-    if (authHeader && authHeader === `Bearer ${apiKey}`) {
+    if (authHeader.startsWith('Bearer ')) {
+      apiKeyToken = authHeader.substring(7);
+    } else {
+      throw new Error('Invalid bearer token format');
+    }
+
+    const apiKeyEntry = await this.serverApiKeysService.findByServerApiKey(apiKeyToken);
+
+    if (apiKeyToken && apiKeyToken === apiKeyEntry.apiKey) {
       return true;
     }
 
