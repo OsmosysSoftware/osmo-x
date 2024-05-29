b6157722a0e446e504d0044ecee75e32ce204bd4
Harish-osmosys <121787291+Harish-osmosys@users.noreply.github.com>
2024-05-29 11:02:42 +0530

    refactor: create separate kaps seed migration file

--- apps/api/src/database/migrations/1692870736646-seeddata.ts
+++ apps/api/src/database/migrations/1692870736646-seeddata.ts
@@ -85,15 +85,6 @@ export class SeedData1692870736646 implements MigrationInterface {
       ],
     );
 
-    await queryRunner.query(
-      `INSERT INTO notify_master_providers (name,provider_type,configuration) VALUES (?, ?, ?)`,
-      [
-        'SMS_KAPSYSTEM',
-        2,
-        '{"KAP_SMS_BASE_API_URL": {"lable": "KAP SMS BASE API URL","id": "KAP_SMS_BASE_API_URL","pattern": "^https?://[^(\\?\\s)]+$","type": "string"},"KAP_SMS_ACCOUNT_USERNAME": {"lable": "KAP SMS ACCOUNT USERNAME","id": "KAP_SMS_ACCOUNT_USERNAME","pattern": "^[a-zA-Z0-9_-]{3,30}$","type": "string"},"KAP_SMS_ACCOUNT_PASSWORD": {"lable": "KAP_SMS_ACCOUNT_PASSWORD","id": "KAP SMS ACCOUNT PASSWORD","pattern": "^[a-zA-Z0-9_-]{3,30}$","type": "string"},"KAP_SMS_FROM": {"lable": "KAP SMS FROM","id": "KAP_SMS_FROM","pattern": "^[a-zA-Z]{6}$","type": "string"}}',
-      ],
-    );
-
     // Add foreign key for channel_type -> master_id
     // Adding this after seeding data in master_providers so FK constraint does not fail
     await queryRunner.createForeignKey(
---
+++ apps/api/src/database/migrations/1692870736647-kapsseeddata.ts
@@ -0,0 +1,23 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class KapsSeedData1692870736647 implements MigrationInterface {
+  name = 'KapsSeedData1692870736647';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `INSERT INTO notify_master_providers (name,provider_type,configuration) VALUES (?, ?, ?)`,
+      [
+        'SMS_KAPSYSTEM',
+        2,
+        '{"KAP_SMS_BASE_API_URL": {"lable": "KAP SMS BASE API URL","id": "KAP_SMS_BASE_API_URL","pattern": "^https?://[^(\\?\\s)]+$","type": "string"},"KAP_SMS_ACCOUNT_USERNAME": {"lable": "KAP SMS ACCOUNT USERNAME","id": "KAP_SMS_ACCOUNT_USERNAME","pattern": "^[a-zA-Z0-9_-]{3,30}$","type": "string"},"KAP_SMS_ACCOUNT_PASSWORD": {"lable": "KAP_SMS_ACCOUNT_PASSWORD","id": "KAP SMS ACCOUNT PASSWORD","pattern": "^[a-zA-Z0-9_-]{3,30}$","type": "string"},"KAP_SMS_FROM": {"lable": "KAP SMS FROM","id": "KAP_SMS_FROM","pattern": "^[a-zA-Z]{6}$","type": "string"}}',
+      ],
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+      DELETE FROM notify_master_providers
+      WHERE id IN (8);
+    `);
+  }
+}
