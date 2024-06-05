0f44714252ba97b987967d6f75a8e572cedd581c
kshitij-k-osmosys <kshitij.k@osmosys.co>
2024-06-05 14:04:45 +0530

    refactor: api-key-guard.spec

--- apps/api/src/common/guards/api-key/api-key.guard.spec.ts
+++ apps/api/src/common/guards/api-key/api-key.guard.spec.ts
@@ -1,10 +1,16 @@
 import { ServerApiKeysService } from 'src/modules/server-api-keys/server-api-keys.service';
 import { ApiKeyGuard } from './api-key.guard';
+import { Logger } from '@nestjs/common';
+import { ProvidersService } from 'src/modules/providers/providers.service';
 
 describe('ApiKeyGuard', () => {
   it('should be defined', () => {
     // Mock or stub ServerApiKeysService as needed
     const serverApiKeysServiceMock = {} as ServerApiKeysService;
-    expect(new ApiKeyGuard(serverApiKeysServiceMock)).toBeDefined();
+    const providersServiceMock = {} as ProvidersService;
+    const loggerMock = {} as Logger;
+    expect(
+      new ApiKeyGuard(serverApiKeysServiceMock, providersServiceMock, loggerMock),
+    ).toBeDefined();
   });
 });
