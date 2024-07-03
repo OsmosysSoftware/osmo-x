df3d4f4b53bc71575ba490aff3a91d5ee5ec6f2b
kshitij-k-osmosys <121787214+kshitij-k-osmosys@users.noreply.github.com>
2024-07-03 14:19:46 +0530

    feat: add retry for 360dialog (#268)

--- apps/api/src/common/constants/notifications.ts
+++ apps/api/src/common/constants/notifications.ts
@@ -36,10 +36,17 @@ export const ProviderDeliveryStatus = {
     SUCCESS_STATES: ['sent', 'delivered', 'read'],
     FAILURE_STATES: ['failed', 'undelivered'],
   },
+  WA_360_DAILOG: {
+    SUCCESS_STATES: ['sent', 'delivered', 'read'],
+    FAILURE_STATES: ['failed'],
+  },
   SMS_KAPSYSTEM: {
     SUCCESS_STATES: ['DELIVRD'],
     FAILURE_STATES: ['EXPIRED', 'UNDELIV', 'FAILED'],
   },
 };
 
-export const SkipProviderConfirmationChannels = [ChannelType.SMS_KAPSYSTEM];
+export const SkipProviderConfirmationChannels = [
+  ChannelType.SMS_KAPSYSTEM,
+  ChannelType.WA_360_DAILOG,
+];
