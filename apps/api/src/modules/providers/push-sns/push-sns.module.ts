26529a2e2e42cb4a272bf5730414b4c32ebbaaed
Harish-osmosys <121787291+Harish-osmosys@users.noreply.github.com>
2024-07-10 17:39:54 +0530

    feat: add support for push notification using sns (#269)

---
+++ apps/api/src/modules/providers/push-sns/push-sns.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { PushSnsService } from './push-sns.service';
+import { ConfigModule } from '@nestjs/config';
+import { ProvidersModule } from '../providers.module';
+import { ProvidersService } from '../providers.service';
+
+@Module({
+  imports: [ConfigModule, ProvidersModule],
+  providers: [PushSnsService, ProvidersService],
+  exports: [PushSnsService],
+})
+export class PushSnsModule {}
