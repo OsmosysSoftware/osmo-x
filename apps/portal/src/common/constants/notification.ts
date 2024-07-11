12a10961c48541b95aa70e3028377f53b3b5f48e
kshitij-k-osmosys <121787214+kshitij-k-osmosys@users.noreply.github.com>
2024-07-11 15:02:53 +0530

    feat: display sns provider & icon in portal (#276)

--- apps/portal/src/common/constants/notification.ts
+++ apps/portal/src/common/constants/notification.ts
@@ -16,6 +16,7 @@ export const ChannelType = {
   SMS_PLIVO: 6,
   WA_TWILIO_BUSINESS: 7,
   SMS_KAPSYSTEM: 8,
+  PUSH_SNS: 9,
 };
 
 export const ChannelTypeMap = {
@@ -74,5 +75,12 @@ export const ChannelTypeMap = {
     providerName: 'KAP System',
     providerIcon: 'assets/icons/kaps.png',
     style: 'sms',
+  },
+  [ChannelType.PUSH_SNS]: {
+    serviceIcon: 'assets/icons/pushnotification.svg',
+    altText: 'Push Notification',
+    providerName: 'AWS SNS',
+    providerIcon: 'assets/icons/aws.png',
+    style: 'pushsns',
   },
 };
