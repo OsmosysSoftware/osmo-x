012a807305fe3279449dec048daf8c9f2d93a362
Harish-osmosys <121787291+Harish-osmosys@users.noreply.github.com>
2024-07-15 10:35:36 +0530

    fix: update webhook module

--- apps/api/src/modules/webhook/entities/webhook.entity.ts
+++ apps/api/src/modules/webhook/entities/webhook.entity.ts
@@ -1,6 +1,6 @@
 import { IsEnum } from 'class-validator';
 import { Status } from 'src/common/constants/database';
-import { Field } from '@nestjs/graphql';
+import { Field, ObjectType } from '@nestjs/graphql';
 import {
   Entity,
   Column,
@@ -10,23 +10,30 @@ import {
 } from 'typeorm';
 
 @Entity('notify_webhooks')
+@ObjectType()
 export class Webhook {
   @PrimaryGeneratedColumn()
+  @Field()
   id: number;
 
   @Column({ name: 'provider_id' })
+  @Field()
   providerId: number;
 
   @Column()
+  @Field()
   webhookUrl: string;
 
   @Column({ name: 'is_verified', default: false })
+  @Field()
   isVerified: number;
 
   @CreateDateColumn({ name: 'created_on' })
+  @Field()
   createdOn: Date;
 
   @UpdateDateColumn({ name: 'updated_on' })
+  @Field()
   updatedOn: Date;
 
   @Column({
