73b2066683d390460ca2717aebd172ee24f767be
kshitij-k-osmosys <kshitij.k@osmosys.co>
2024-07-16 23:12:39 +0530

    fix: vc twilio dto

--- apps/api/src/modules/notifications/dtos/providers/vcTwilio-data.dto.ts
+++ apps/api/src/modules/notifications/dtos/providers/vcTwilio-data.dto.ts
@@ -9,6 +9,38 @@ export class VcTwilioDataDto {
 
   // Need atleast one of url, twiml for successful request
   url?: string;
-
   twiml?: string;
+
+  // Remaining parameters
+  method?: string;
+  fallbackUrl?: string;
+  fallbackMethod?: string;
+  statusCallback?: string;
+  statusCallbackEvent?: Array<string>;
+  statusCallbackMethod?: string;
+  sendDigits?: string;
+  timeout?: number;
+  record?: boolean;
+  recordingChannels?: string;
+  recordingStatusCallback?: string;
+  recordingStatusCallbackMethod?: string;
+  sipAuthUsername?: string;
+  sipAuthPassword?: string;
+  machineDetection?: string;
+  machineDetectionTimeout?: number;
+  recordingStatusCallbackEvent?: Array<string>;
+  trim?: string;
+  callerId?: string;
+  machineDetectionSpeechThreshold?: number;
+  machineDetectionSpeechEndThreshold?: number;
+  machineDetectionSilenceTimeout?: number;
+  asyncAmd?: string;
+  asyncAmdStatusCallback?: string;
+  asyncAmdStatusCallbackMethod?: string;
+  byoc?: string;
+  callReason?: string;
+  callToken?: string;
+  recordingTrack?: string;
+  timeLimit?: number;
+  applicationSid?: string;
 }
