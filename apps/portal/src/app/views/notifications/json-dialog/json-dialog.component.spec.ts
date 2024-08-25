a11820d6ef7e5331f2a7ad27594dac0e78168178
kshitij-k-osmosys <kshitij.k@osmosys.co>
2024-08-25 15:25:59 +0530

    feat: testcase for copyToClipboard()

--- apps/portal/src/app/views/notifications/json-dialog/json-dialog.component.spec.ts
+++ apps/portal/src/app/views/notifications/json-dialog/json-dialog.component.spec.ts
@@ -1,21 +1,56 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TestBed } from '@angular/core/testing';
+import { Clipboard } from '@angular/cdk/clipboard';
+import { MessageService } from 'primeng/api';
 import { JsonDialogComponent } from './json-dialog.component';
 
 describe('JsonDialogComponent', () => {
   let component: JsonDialogComponent;
-  let fixture: ComponentFixture<JsonDialogComponent>;
+  let clipboardSpy: jasmine.SpyObj<Clipboard>;
+  let messageServiceSpy: jasmine.SpyObj<MessageService>;
 
   beforeEach(() => {
+    const clipboard = jasmine.createSpyObj('Clipboard', ['copy']);
+    const messageService = jasmine.createSpyObj('MessageService', ['add']);
+
     TestBed.configureTestingModule({
       declarations: [JsonDialogComponent],
+      providers: [
+        { provide: Clipboard, useValue: clipboard },
+        { provide: MessageService, useValue: messageService },
+      ],
     });
-    fixture = TestBed.createComponent(JsonDialogComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+
+    component = TestBed.createComponent(JsonDialogComponent).componentInstance;
+    clipboardSpy = TestBed.inject(Clipboard) as jasmine.SpyObj<Clipboard>;
+    messageServiceSpy = TestBed.inject(MessageService) as jasmine.SpyObj<MessageService>;
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should copy JSON data to clipboard and show success message', () => {
+    component.jsonData = { key: 'value' };
+    clipboardSpy.copy.and.returnValue(true);
+
+    component.copyToClipboard();
+
+    expect(clipboardSpy.copy).toHaveBeenCalledWith(JSON.stringify(component.jsonData));
+    expect(messageServiceSpy.add).toHaveBeenCalledWith({
+      key: 'tst',
+      severity: 'info',
+      summary: 'Info',
+      detail: 'JSON data copied to clipboard!',
+    });
+  });
+
+  it('should show error message if copying fails', () => {
+    component.jsonData = { key: 'value' };
+    clipboardSpy.copy.and.throwError('Copy failed');
+
+    component.copyToClipboard();
+
+    expect(messageServiceSpy.add).toHaveBeenCalledWith({
+      key: 'tst',
+      severity: 'error',
+      summary: 'Error',
+      detail: 'There was an error copying JSON data. Reason: Copy failed',
+    });
   });
 });
