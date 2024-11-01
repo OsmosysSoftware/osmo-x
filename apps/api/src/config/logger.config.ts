6fb9a2bdb9f0f279164ff9295d54a7acc2da47b2
kshitij-k-osmosys <kshitij.k@osmosys.co>
2024-11-01 14:51:22 +0530

    feat: logfile maxSize from env

--- apps/api/src/config/logger.config.ts
+++ apps/api/src/config/logger.config.ts
@@ -5,10 +5,16 @@ import { v4 as uuidv4 } from 'uuid';
 import 'winston-daily-rotate-file';
 import { ConfigService } from '@nestjs/config';
 import { name as applicationName } from 'package.json';
+import DailyRotateFile from 'winston-daily-rotate-file';
 
 const configService = new ConfigService();
 
 const logDir = 'logs';
+
+// Fetch maxSize values from environment variables or use defaults
+const combinedLogMaxSize = configService.get<string>('COMBINED_LOG_MAX_SIZE');
+const errorLogMaxSize = configService.get<string>('ERROR_LOG_MAX_SIZE');
+
 const logFormat = format.combine(
   format.timestamp(),
   format.printf((info) => {
@@ -32,6 +38,45 @@ const logFormat = format.combine(
   }),
 );
 
+// Configure combined log options
+const combinedLogOptions: DailyRotateFile.DailyRotateFileTransportOptions = {
+  filename: join(logDir, 'combined-%DATE%.log'),
+  datePattern: 'YYYY-MM-DD',
+  zippedArchive: true,
+  maxFiles: '30d',
+  format: logFormat,
+};
+
+// Set maxSize for combined logs based on env variable or default
+if (combinedLogMaxSize === '0') {
+  // Do not set maxSize to remove the limit
+} else if (combinedLogMaxSize) {
+  combinedLogOptions.maxSize = combinedLogMaxSize;
+} else {
+  // Default value
+  combinedLogOptions.maxSize = '150m';
+}
+
+// Configure error log options
+const errorLogOptions: DailyRotateFile.DailyRotateFileTransportOptions = {
+  filename: join(logDir, 'error-%DATE%.log'),
+  level: 'error',
+  datePattern: 'YYYY-MM-DD',
+  zippedArchive: true,
+  maxFiles: '30d',
+  format: logFormat,
+};
+
+// Set maxSize for error logs based on env variable or default
+if (errorLogMaxSize === '0') {
+  // Do not set maxSize to remove the limit
+} else if (errorLogMaxSize) {
+  errorLogOptions.maxSize = errorLogMaxSize;
+} else {
+  // Default value
+  errorLogOptions.maxSize = '20m';
+}
+
 const transportsConfig = [
   new transports.Console({
     format: format.combine(
@@ -40,24 +85,8 @@ const transportsConfig = [
       nestWinstonModuleUtilities.format.nestLike(applicationName),
     ),
   }),
-
-  new transports.DailyRotateFile({
-    filename: join(logDir, 'combined-%DATE%.log'),
-    datePattern: 'YYYY-MM-DD',
-    zippedArchive: true,
-    maxFiles: '30d',
-    maxSize: '150m',
-    format: logFormat,
-  }),
-  new transports.DailyRotateFile({
-    filename: join(logDir, 'error-%DATE%.log'),
-    level: 'error',
-    datePattern: 'YYYY-MM-DD',
-    zippedArchive: true,
-    maxFiles: '30d',
-    maxSize: '20m',
-    format: logFormat,
-  }),
+  new transports.DailyRotateFile(combinedLogOptions),
+  new transports.DailyRotateFile(errorLogOptions),
 ];
 
 export const loggerConfig = WinstonModule.createLogger({
