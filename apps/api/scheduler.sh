4fc1d7e61ae16deffe40166afdceab4ab862d8a3
kshitij-k-osmosys <kshitij.k@osmosys.co>
2024-11-20 14:52:08 +0530

    feat: simplify scheduler script

--- apps/api/scheduler.sh
+++ apps/api/scheduler.sh
@@ -5,9 +5,9 @@ SCHEDULE_TIME=5
 source ".env"
 
 BASE_URL="http://localhost:${SERVER_PORT}/notifications"
-
 ARCHIVE_URL="http://localhost:${SERVER_PORT}/archived-notifications"
-ARCHIVE_INTERVAL_SCHEDULE_TIME="${ARCHIVE_INTERVAL:-3600}"
+
+ARCHIVE_INTERVAL="${ARCHIVE_INTERVAL:-3600}"
 
 add_notifications_to_queue() {
   curl -X POST "${BASE_URL}/queue" -H "Content-Type: application/json" -d '{}'
@@ -21,22 +21,19 @@ move_completed_notifications_to_archive() {
   curl -X POST "${ARCHIVE_URL}/archive" -H "Content-Type: application/json" -d '{}'
 }
 
-last_run_notifications=$(date +%s)
-last_run_archived_notifications=$(date +%s)
+last_archive_run=$(date +%s)
 
 while true; do
+  add_notifications_to_queue
+  get_provider_confirmation
+
   current_time=$(date +%s)
 
-  if (( (current_time - last_run_notifications) >= SCHEDULE_TIME )); then
-    add_notifications_to_queue
-    get_provider_confirmation
-    last_run_notifications=$current_time
-  fi
-
-  if (( (current_time - last_run_archived_notifications) >= ARCHIVE_INTERVAL_SCHEDULE_TIME )); then
+  # Check if it's time to run the archive function
+  if (( (current_time - last_archive_run) >= ARCHIVE_INTERVAL )); then
     move_completed_notifications_to_archive
-    last_run_archived_notifications=$current_time
+    last_archive_run=$current_time
   fi
 
-  sleep 1
+  sleep $SCHEDULE_TIME
 done
