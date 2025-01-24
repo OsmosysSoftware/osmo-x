c14ba9bc93243a426b9a8a0539a930187c129437
kshitij-k-osmosys <121787214+kshitij-k-osmosys@users.noreply.github.com>
2025-01-24 20:57:50 +0530

    fix: use dhilog instead of slogerr (#388)

--- apps/api/src/common/logger/dhilog.transport.ts
+++ apps/api/src/common/logger/dhilog.transport.ts
@@ -20,10 +20,10 @@ interface LogInfo {
   severity: string;
 }
 
-export class SlogerrTransport extends TransportStream {
+export class DhilogTransport extends TransportStream {
   private readonly httpService: HttpService;
   private readonly configService: ConfigService;
-  private readonly logger = new Logger(SlogerrTransport.name);
+  private readonly logger = new Logger(DhilogTransport.name);
 
   constructor(options: CustomTransportOptions) {
     super(options);
@@ -32,14 +32,14 @@ export class SlogerrTransport extends TransportStream {
   }
 
   async log(info: LogInfo, callback: () => void): Promise<void> {
-    const allowedLevels = (this.configService.get<string>('SLOGGER_LOG_LEVEL') || 'error')
+    const allowedLevels = (this.configService.get<string>('DHILOG_LOG_LEVEL') || 'error')
       .split(',')
       .map((level) => level.trim());
-    const logType = this.configService.get<string>('SLOGGER_LOG_TYPE') || 'Exceptions';
+    const logType = this.configService.get<string>('DHILOG_LOG_TYPE') || 'Exceptions';
 
     if (allowedLevels.includes(info.level)) {
-      const apiEndpoint = this.configService.get<string>('SLOGERR_API_ENDPOINT');
-      const apiKey = this.configService.get<string>('SLOGERR_API_TOKEN');
+      const apiEndpoint = this.configService.get<string>('DHILOG_API_ENDPOINT');
+      const apiKey = this.configService.get<string>('DHILOG_API_TOKEN');
 
       this.logger.log(`Log Info: ${stringify(info)}`);
 
@@ -67,13 +67,13 @@ export class SlogerrTransport extends TransportStream {
 
         if (response.status !== 200) {
           this.logger.warn(
-            `Failed to send log to Slogerr. Status: ${response.status}, Message: ${response.statusText}`,
+            `Failed to send log to Dhilog. Status: ${response.status}, Message: ${response.statusText}`,
           );
         } else {
-          this.logger.log('Error log successfully sent to Slogerr', response);
+          this.logger.log('Error log successfully sent to Dhilog', response);
         }
       } catch (error) {
-        this.logger.warn('Failed to send log to Slogerr', error.message);
+        this.logger.warn('Failed to send log to Dhilog', error.message);
       }
     }
 
