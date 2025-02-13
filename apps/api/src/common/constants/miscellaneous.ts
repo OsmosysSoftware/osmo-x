498af53d86611751779914f2ae46ce3b004941f3
kshitij-k-osmosys <kshitij.k@osmosys.co>
2025-02-13 19:57:54 +0530

    style: update test mode result message

--- apps/api/src/common/constants/miscellaneous.ts
+++ apps/api/src/common/constants/miscellaneous.ts
@@ -1,3 +1,3 @@
 export const SUCCESS_RESPONSE = `🚀✨ You're all set! Everything is up and running smoothly! ✨🚀`;
-const testModeResultString = `{"result": "This is a test mode notification. Notification was not sent. The recipient did not receive the message."}`;
+const testModeResultString = `{"result": "This is a test mode notification. Notification was not delivered to recipient."}`;
 export const TEST_MODE_RESULT_JSON = JSON.parse(testModeResultString.trim());
