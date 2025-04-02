c2f54ab15e58bf1a1286f34fe769cb2c0cd87297
kshitij-k-osmosys <121787214+kshitij-k-osmosys@users.noreply.github.com>
2025-04-02 10:46:05 +0530

    feat: add migration for notification sent on column (#408)

---
+++ apps/api/src/database/migrations/1743145236864-AddNotificationSentOnColumn.ts
@@ -0,0 +1,39 @@
+import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
+
+export class AddNotificationSentOnColumn1743145236864 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'notify_archived_notifications',
+      new TableColumn({
+        name: 'notification_sent_on',
+        type: 'timestamp',
+        isNullable: true,
+      }),
+    );
+
+    await queryRunner.query(`
+      UPDATE notify_archived_notifications
+      SET notification_sent_on = updated_on;
+    `);
+
+    await queryRunner.addColumn(
+      'notify_notifications',
+      new TableColumn({
+        name: 'notification_sent_on',
+        type: 'timestamp',
+        isNullable: true,
+      }),
+    );
+
+    await queryRunner.query(`
+      UPDATE notify_notifications
+      SET notification_sent_on = updated_on;
+    `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumn('notify_notifications', 'notification_sent_on');
+
+    await queryRunner.dropColumn('notify_archived_notifications', 'notification_sent_on');
+  }
+}
