1de1424ebcec7eeef832a406e2dee7b13691aabf
kshitij-k-osmosys <kshitij.k@osmosys.co>
2025-05-25 20:40:28 +0530

    feat: update primeng to v18.0.2

--- apps/portal/src/app/app.module.ts
+++ apps/portal/src/app/app.module.ts
@@ -1,10 +1,12 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 import { FormsModule } from '@angular/forms';
 import { MessageService } from 'primeng/api';
 import { CalendarModule } from 'primeng/calendar';
+import { PrimeNG } from 'primeng/config';
+import LaraLightBlue from '@primeng/themes/lara';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NotificationsComponent } from './views/notifications/notifications.component';
@@ -14,6 +16,19 @@ import { PrimeNgModule } from './primeng.module';
 import { LoginComponent } from './auth/login/login.component';
 import { FooterComponent } from './views/footer/footer.component';
 import { GraphQLModule } from './graphql/graphql.module';
+
+// Function to initialize PrimeNG theme
+function initializePrimeNG(primeng: PrimeNG) {
+  return () => {
+    // Set your desired PrimeNG theme preset
+    primeng.theme.set({
+      preset: LaraLightBlue,
+      options: {
+        darkModeSelector: false || 'none',
+      },
+    });
+  };
+}
 
 @NgModule({
   declarations: [AppComponent, NotificationsComponent, JsonDialogComponent, LoginComponent],
@@ -27,7 +42,17 @@ import { GraphQLModule } from './graphql/graphql.module';
     FooterComponent,
     GraphQLModule,
   ],
-  providers: [NotificationsService, MessageService],
+  providers: [
+    NotificationsService,
+    MessageService,
+    PrimeNG,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializePrimeNG,
+      deps: [PrimeNG],
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
