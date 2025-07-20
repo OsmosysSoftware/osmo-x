25373e9224da295c8b39b1d68e667550a58708c9
kshitij-k-osmosys <kshitij.k@osmosys.co>
2025-07-20 18:10:02 +0530

    feat: get first chain member service fn

--- apps/api/src/modules/provider-chain-members/provider-chain-members.service.ts
+++ apps/api/src/modules/provider-chain-members/provider-chain-members.service.ts
@@ -1,4 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ProviderChainMember } from './entities/provider-chain-member.entity';
+import { Repository } from 'typeorm';
+import { Status } from 'src/common/constants/database';
 
 @Injectable()
-export class ProviderChainMembersService {}
+export class ProviderChainMembersService {
+  protected readonly logger = new Logger(ProviderChainMembersService.name);
+  constructor(
+    @InjectRepository(ProviderChainMember)
+    private readonly providerChainMembersRepository: Repository<ProviderChainMember>,
+  ) {}
+
+  async getFirstPriorityProviderIdByChainId(providerChainId: number): Promise<number | null> {
+    const providerChainMemberEntry = await this.providerChainMembersRepository.findOne({
+      where: {
+        chainId: providerChainId,
+        isActive: Status.ACTIVE,
+        status: Status.ACTIVE,
+      },
+      order: {
+        priorityOrder: 'ASC',
+      },
+      select: ['providerId'],
+    });
+
+    return providerChainMemberEntry?.providerId ?? null;
+  }
+}
