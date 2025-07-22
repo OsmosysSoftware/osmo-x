e307ded814a77b7da9a791a7c0ac0dbb4f39b550
kshitij-k-osmosys <kshitij.k@osmosys.co>
2025-07-22 18:38:55 +0530

    feat: create providerChainMembers findAll function

--- apps/api/src/common/graphql/services/core.service.ts
+++ apps/api/src/common/graphql/services/core.service.ts
@@ -47,6 +47,11 @@ export abstract class CoreService<TEntity> {
       queryBuilder.leftJoinAndSelect(`${alias}.applicationDetails`, 'application');
     }
 
+    if (alias === 'providerChainMember') {
+      queryBuilder.leftJoinAndSelect(`${alias}.providerDetails`, 'provider');
+      queryBuilder.leftJoinAndSelect(`${alias}.providerChainDetails`, 'provider-chain');
+    }
+
     // Apply base conditions
     baseConditions.forEach((condition) => {
       queryBuilder.andWhere(`${alias}.${condition.field} = :${condition.field}`, {
---
+++ apps/api/src/modules/provider-chain-members/provider-chain-members.resolver.ts
@@ -0,0 +1,25 @@
+import { Args, Query, Resolver } from '@nestjs/graphql';
+import { ProviderChainMember } from './entities/provider-chain-member.entity';
+import { Roles } from 'src/common/decorators/roles.decorator';
+import { UserRoles } from 'src/common/constants/database';
+import { UseGuards } from '@nestjs/common';
+import { GqlAuthGuard } from 'src/common/guards/api-key/gql-auth.guard';
+import { RolesGuard } from 'src/common/guards/role.guard';
+import { QueryOptionsDto } from 'src/common/graphql/dtos/query-options.dto';
+import { ProviderChainMemberResponse } from './dto/provider-chain-member-response.dto';
+import { ProviderChainMembersService } from './provider-chain-members.service';
+
+@Resolver(() => ProviderChainMember)
+@Roles(UserRoles.ADMIN)
+@UseGuards(GqlAuthGuard, RolesGuard)
+export class ProviderChainMembersResolver {
+  constructor(private readonly providerChainMembersService: ProviderChainMembersService) {}
+
+  @Query(() => ProviderChainMemberResponse, { name: 'providerChainMembers' })
+  async findAll(
+    @Args('options', { type: () => QueryOptionsDto, nullable: true, defaultValue: {} })
+    options: QueryOptionsDto,
+  ): Promise<ProviderChainMemberResponse> {
+    return this.providerChainMembersService.getAllProviderChainMembers(options);
+  }
+}
