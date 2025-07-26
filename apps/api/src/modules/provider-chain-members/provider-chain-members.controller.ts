385dced3bef4fead53f793241e0ad657c0c7405e
kshitij-k-osmosys <kshitij.k@osmosys.co>
2025-07-26 22:51:51 +0530

    feat: add controller for providerChainMembers

---
+++ apps/api/src/modules/provider-chain-members/provider-chain-members.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('provider-chain-members')
+export class ProviderChainMembersController {}
--- apps/api/src/modules/provider-chain-members/provider-chain-members.module.ts
+++ apps/api/src/modules/provider-chain-members/provider-chain-members.module.ts
@@ -1,20 +1,31 @@
-import { Logger, Module } from '@nestjs/common';
+import { forwardRef, Logger, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProviderChainMember } from './entities/provider-chain-member.entity';
 import { ProviderChainMembersService } from './provider-chain-members.service';
 import { ProviderChainMembersResolver } from './provider-chain-members.resolver';
 import { JwtModule, JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
+import { ProviderChainMembersController } from './provider-chain-members.controller';
+import { ProvidersModule } from '../providers/providers.module';
+import { ProviderChainsService } from '../provider-chains/provider-chains.service';
+import { ProviderChainsModule } from '../provider-chains/provider-chains.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ProviderChainMember]), JwtModule],
+  imports: [
+    TypeOrmModule.forFeature([ProviderChainMember]),
+    JwtModule,
+    ProvidersModule,
+    forwardRef(() => ProviderChainsModule),
+  ],
   providers: [
     Logger,
     ProviderChainMembersService,
     ProviderChainMembersResolver,
     JwtService,
     ConfigService,
+    ProviderChainsService,
   ],
   exports: [TypeOrmModule, ProviderChainMembersService],
+  controllers: [ProviderChainMembersController],
 })
 export class ProviderChainMembersModule {}
