533de20481206a3f7b18c7445d26a23294e2cab3
kshitij-k-osmosys <kshitij.k@osmosys.co>
2025-07-27 20:18:34 +0530

    feat: better decorators for update provider priority order

--- apps/api/src/modules/provider-chain-members/dto/update-provider-priority-order.input.ts
+++ apps/api/src/modules/provider-chain-members/dto/update-provider-priority-order.input.ts
@@ -1,9 +1,11 @@
-import { IsArray, IsNumber } from 'class-validator';
+import { ArrayMinSize, IsArray, IsNumber } from 'class-validator';
 
 export class UpdateProviderPriorityOrderInput {
   @IsNumber()
   chainId: number;
 
   @IsArray()
+  @IsNumber({}, { each: true })
+  @ArrayMinSize(1)
   newProviderPriorityOrder: number[];
 }
